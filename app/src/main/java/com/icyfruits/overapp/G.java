7e7cf420ca6cd76f1e3c96ec215e056655491c5a
devcshin <icyfruits@naver.com>
2016-12-01 22:08:37 +0900

    join
    me

--- app/src/main/java/com/icyfruits/overapp/G.java
+++ app/src/main/java/com/icyfruits/overapp/G.java
@@ -5,12 +5,13 @@ package com.icyfruits.overapp;
  */
 
 public class G {
+    
 
     public static String id=new String("");
     public static String check=new String("");
 
     public static int count;
-    
+
     public static String login_id;
     public static String login_pw;
 }
